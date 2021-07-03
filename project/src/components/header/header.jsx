f92a51731c2b3aa1dad7df6eab4c4aae01f3f89e
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-07-03 15:18:15 +0300

    add favorites, login pages

--- project/src/components/header/header.jsx
+++ project/src/components/header/header.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../const';
 
+import Logo from '../logo/logo';
+
 function Header() {
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-
+            <Logo />
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
--- project/src/components/pages/main-page/main-page.jsx
+++ project/src/components/pages/main-page/main-page.jsx
@@ -9,7 +9,9 @@ function MainPage(props) {
 
   return (
     <div className="page page--gray page--main">
+
       <Header />
+
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
