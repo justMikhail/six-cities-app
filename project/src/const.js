e92b4bc9ad66e08b4ee688725bdab6d7517de367
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-07-22 01:58:36 +0300

    update "index.js" with redux tools

--- project/src/const.js
+++ project/src/const.js
@@ -30,33 +30,12 @@ export const PlaceCardPageType = {
   },
 };
 
+export const CITIES = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
+
 export const Color = {
   FAVORITE_CHECKED: '#4481c3',
   FAVORITE_NOT_CHECKED: '#979797',
 };
-
-//export const Ratings = [
-  //  {
-//    title: 'perfect',
-//    id: 5,
-//  },
-//  {
-//    title: 'good',
-//    id: 4,
-//  },
-//  {
-//    title: 'not bad',
-//    id: 3,
-//  },
-//  {
-//    title: 'badly',
-//    id: 2,
-//  },
-//  {
-//    title: 'terribly',
-//    id: 1,
-//  },
-//];
 
 export const Ratings = {
   ONE: {
@@ -80,4 +59,3 @@ export const Ratings = {
     title: 'perfect',
   },
 };
-
--- project/src/index.js
+++ project/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 import App from './components/app/app';
 
 import offersMocks from './mocks/offers';
 import reviewsMocks from './mocks/reviews';
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App
-      offers = { offersMocks }
-      reviews = { reviewsMocks }
-    />,
+  <React.StrictMode>\
+    <Provider store={store}>
+      <App
+        offers = { offersMocks }
+        reviews = { reviewsMocks }
+      />,
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
